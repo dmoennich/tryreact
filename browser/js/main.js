78351a84755ea707160a19494e21d84a08795b89
Daniel Moennich <daniel.moennich@gmail.com>
2015-11-04 18:37:58 +0100

    More react classes.

--- browser/js/main.js
+++ browser/js/main.js
@@ -4,7 +4,53 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
+
+let Comment = React.createClass({
+    render: function () {
+       return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+
+let CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">The form</div>
+        );
+    }
+});
+
+let CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                <Comment author="Daniel">This is a comment!</Comment>
+                <Comment author="some dude">This is *another* comment!</Comment>
+            </div>
+        );
+    }
+});
+
+let CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-<h1>KEYS OR NOT</h1>,
+    <CommentBox />,
     document.getElementById('content')
 );
